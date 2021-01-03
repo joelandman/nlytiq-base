9820653b477e97a07c08769a81f1962a31dec48f
Joe Landman <joe@nlytiq.com>
2021-01-03 15:09:47 -0500

    Added maxima 5.44.0 (Computer Algebra System) to the tools. Removed some Perl modules I never use anymore. Updated prep scripts to include some dependencies needed for maxima (sbcl, a lisp implementation)

--- prep_ubuntu.sh
+++ prep_ubuntu.sh
@@ -9,4 +9,4 @@ apt-get install -y libffi-dev gfortran gfortran-8 libarpack2 libarpack2-dev   \
        tk-dev libtclcl1-dev libplplot-dev tcl-dev tclcl-dev tcl8.6-dev        \
        libtinfo-dev libtexttools8-dev libncurses5-dev      \
        libqdbm-dev libgdbm-dev libsqlite3-dev jq libbz2-dev zlib1g-dev        \
-       libncurses-dev make libpcre2-dev libpcre3-dev libzmq3-dev
+       libncurses-dev make libpcre2-dev libpcre3-dev libzmq3-dev sbcl texinfo

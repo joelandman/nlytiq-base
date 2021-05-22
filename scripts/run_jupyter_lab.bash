6482406993c134c16988508192dbbda4b41b80f5
Joe Landman <joe@nlytiq.com>
2021-05-22 13:27:00 -0400

    Perl updated to 5.34.0

--- scripts/run_jupyter_lab.bash
+++ scripts/run_jupyter_lab.bash
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-jupyter lab --ip 0.0.0.0
+# this will use token
+# jupyter lab --ip 0.0.0.0
+
+# this will use password
+jupyter lab --ip 0.0.0.0 --port 8888 --no-browser
+#
+# set up the password by following the below steps
+#    jupyter notebook --generate-config
+#    jupyter notebook password
+

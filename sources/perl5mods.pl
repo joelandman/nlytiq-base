819c1c1445477738abd32e133b7a64637645726c
Joe Landman <joe@nlytiq.com>
2023-07-06 10:21:40 -0400

    Update Julia to 1.9.2, Perl to 5.38.0

--- sources/perl5mods.pl
+++ sources/perl5mods.pl
@@ -5,23 +5,45 @@ use CPAN;
 use MCE::Loop;
 
 my @mods=qw(
-        Digest::SHA IO::Pty IPC::Cmd IO::Socket::SSL
-	IPC::Run File::Spec File::PathConvert ZMQ::LibZMQ3
-	Getopt::Lucid JSON::PP DBI DBD::SQLite Digest::SHA1
-	File::ChangeNotify Data::Serializer Time::ParseDate Moo
-	Text::ASCIITable Text::UnicodeTable::Simple Text::Table
-	Module::Install Module::AutoInstall Module::Build Starman
-	Mojolicious XML::Smart
-	Math::Random::Secure
-	Crypt::PRNG MCE MCE::Loop MCE::Flow
-	MCE::Map MCE::Grep MCE::Queue MCE::Hobo MCE::Shared
-	HTTP::Response HTML::Entities
-	URI::Escape 
-	ZMQ::FFI SVG::Sparkline
+        Digest::SHA
+		Digest::SHA1
+		Module::Install
+		Module::AutoInstall 
+		Module::Build 
+		MCE 
+		MCE::Queue 
+		MCE::Shared 
+		Moo
+		HTTP::Response 
+		HTML::Entities
+		URI::Escape 
+		Starman
+		Mojolicious 
+		Data::Serializer
+		IO::Pty 
+		IPC::Cmd 
+		IO::Socket::SSL
+		IPC::Run 
+		File::Spec 
+		File::PathConvert 
+		Getopt::Lucid
+		JSON::PP
+		DBI
+		DBD::SQLite		
+		File::ChangeNotify
+		Time::ParseDate
+		Text::ASCIITable 
+		Text::UnicodeTable::Simple 
+		Text::Table
+		Math::Random::Secure 
+		Crypt::PRNG 	
+		ZMQ::LibZMQ3	
+		ZMQ::FFI
+		SVG::Sparkline
 );
 
 MCE::Loop->init(
-  max_workers => 8, chunk_size => 1
+  max_workers => 8, chunk_size => 3
 );
 
 mce_loop {
